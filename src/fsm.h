2a1d2b3a4518f63a68b29f06f8c162e41da3f650
Elham Shahriari <143998115+ELHAMsh1988@users.noreply.github.com>
2026-01-07 21:36:09 +0330

    Update fsm.h

--- src/fsm.h
+++ src/fsm.h
@@ -1,9 +1,21 @@
+#ifndef FSM_H
+#define FSM_H
+
 typedef enum {
     STATE_IDLE,
-    STATE_ACTIVE,
-    STATE_ERROR
+    STATE_RUNNING
 } system_state_t;
 
-void fsm_init(void);
-void fsm_update(void);
+typedef enum {
+    EVENT_START,
+    EVENT_STOP
+} Event;
 
+typedef struct {
+    system_state_t state;
+} FSM;
+
+void fsm_transition(FSM *fsm, Event event);
+void fsm_print_state(FSM *fsm);
+
+#endif
